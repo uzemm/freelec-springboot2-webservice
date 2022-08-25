ff81e1fe8ae5a1d6d1f163f72c2b65497282ac01
yujin <100543186+uzemm@users.noreply.github.com>
2022-08-25 16:22:37 +0900

    구글 로그인 연동, 스프링 시큐리티 설정

---
+++ src/main/java/com/uzem/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.uzem.book.springboot.config.auth;
+
+
+import com.uzem.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserTypesOAuth2UserService customUserTypesOAuth2UserService;
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customUserTypesOAuth2UserService);
+    }
+
+}
