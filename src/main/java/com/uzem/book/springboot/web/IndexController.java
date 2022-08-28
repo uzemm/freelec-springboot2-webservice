de432bbc934875288e421a3a29b267d087b702fe
yujin <100543186+uzemm@users.noreply.github.com>
2022-08-28 13:44:00 +0900

    어노테이션 기반으로 개선하기

--- src/main/java/com/uzem/book/springboot/web/IndexController.java
+++ src/main/java/com/uzem/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.uzem.book.springboot.web;
 
+import com.uzem.book.springboot.config.auth.LoginUser;
 import com.uzem.book.springboot.config.auth.dto.SessionUser;
 import com.uzem.book.springboot.service.PostsService;
 import com.uzem.book.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("loginUserName", user.getName());
         }
